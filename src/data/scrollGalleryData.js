62ca08ee6708c09acf683e4199ee596a83274d3c
Chibuokem Egbuchulam <talk2chibu@gmail.com>
2022-11-15 23:46:08 +0100

    style: update scroll gallery images with new one

--- src/data/scrollGalleryData.js
+++ src/data/scrollGalleryData.js
@@ -1,17 +1,19 @@
-import img1 from "../assets/img1.webp";
-import img2 from "../assets/img2.webp";
-import img3 from "../assets/img3.webp";
-import img4 from "../assets/img4.webp";
-import img5 from "../assets/img5.webp";
-import img6 from "../assets/img6.webp";
-import img7 from "../assets/img7.webp";
+import img1 from "../assets/scroll-bg1.jpg";
+import img3 from "../assets/scroll-bg3.jpg";
+import img4 from "../assets/scroll-bg4.jpg";
+import img5 from "../assets/scroll-bg5.jpg";
+import img6 from "../assets/scroll-bg6.jpg";
+import img7 from "../assets/scroll-bg7.jpg";
+import img9 from "../assets/scroll-bg9.jpg";
+import img10 from "../assets/scroll-bg10.jpg";
+import img11 from "../assets/scroll-bg11.jpg";
+import img13 from "../assets/scroll-bg13.jpg";
+import img14 from "../assets/scroll-bg14.webp";
+import img15 from "../assets/scroll-bg12.jpg";
 
 const scrollData = [
   {
     img: img1,
-  },
-  {
-    img: img2,
   },
   {
     img: img3,
@@ -23,10 +25,28 @@ const scrollData = [
     img: img5,
   },
   {
+    img: img7,
+  },
+  {
+    img: img9,
+  },
+  {
+    img: img10,
+  },
+  {
+    img: img11,
+  },
+  {
+    img: img13,
+  },
+  {
     img: img6,
   },
   {
-    img: img7,
+    img: img14,
+  },
+  {
+    img: img15,
   },
 ];
 export default scrollData;
